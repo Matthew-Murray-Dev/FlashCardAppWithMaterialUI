dbe8de1df82d2c74b32743560f9d6bf4a5662e53
Matthew Murray <matthewc.murray94@gmail.com>
2023-04-11 21:46:56 -0500

    minor formatting

--- src/Layout/Buttons/DeleteCardButton.js
+++ src/Layout/Buttons/DeleteCardButton.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { Button } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { useRouteMatch, useHistory } from "react-router-dom";
 import { deleteCard } from "../../utils/api";
 
-function DeleteCardButton({id}){
-  const {url}=useRouteMatch();
-  const history= useHistory();
+function DeleteCardButton({ id }) {
+  const { url } = useRouteMatch();
+  const history = useHistory();
   const handleDelete = (event) => {
-    event.preventDefault()
-   windowConfirm()
-   history.go(0)
-   
-}
-const windowConfirm=()=>{window.confirm(`Delete this card?\n\nYou will not be able to recover it.`)? deleteCard(id):history.push(url)}
-    return (
+    event.preventDefault();
+    windowConfirm();
+    history.go(0);
+  };
+  const windowConfirm = () => {
+    window.confirm(`Delete this card?\n\nYou will not be able to recover it.`)
+      ? deleteCard(id)
+      : history.push(url);
+  };
+  return (
     <div className="DeleteButton float-right">
-      <Button size="large" type="button" className="btn pr-2" style={{height:"36.5px",maxWidth: '36px',  minWidth: '36px' ,"backgroundColor":"#D9534f"}}
-        variant="contained" startIcon={<DeleteIcon/>} onClick={handleDelete}></Button>
+      <Button
+        size="large"
+        type="button"
+        className="btn pr-2"
+        style={{
+          height: "36.5px",
+          maxWidth: "36px",
+          minWidth: "36px",
+          backgroundColor: "#D9534f",
+        }}
+        variant="contained"
+        startIcon={<DeleteIcon />}
+        onClick={handleDelete}
+      ></Button>
     </div>
   );
 
